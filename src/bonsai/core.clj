747983267d98039060ba8818e9e0c8b8ac01babc
Oliver Caldwell <olliec87@gmail.com>
2017-08-12 11:57:39 +0100

    Added helper macros and split up handle!

--- src/bonsai/core.clj
+++ src/bonsai/core.clj
@@ -5,47 +5,58 @@
    ::!event-handlers (atom {})
    ::!effect-handlers (atom {})})
 
-;; Maybe make this register-event! and wrap it in a nice macro.
-(defn event! [app key f]
+(defn define-event! [app key f]
   (swap! (::!event-handlers app) #(assoc % key f)))
 
-;; Same goes for this.
-;; defeffect and defevent should be good.
-(defn effect! [app key f]
+(defn define-effect! [app key f]
   (swap! (::!effect-handlers app) #(assoc % key f)))
 
-;; Break this down into other functions.
-(defn handle! [app key & args]
-  (let [f (-> app ::!event-handlers deref key)
-        state (swap! (::!state app) #(apply f % args))
-        effects (-> state meta ::effects)]
-    (doseq [[effect args] effects]
-      (let [f (-> app ::!effect-handlers deref effect)]
-        (apply f (partial handle! app) args)))
-    (swap! (::!state app) #(with-meta % (dissoc (meta %) ::effects)))
-    app))
+(defmacro defevent [app key args body]
+  `(define-event! ~app ~key (fn ~args ~body)))
+
+(defmacro defeffect [app key args body]
+  `(define-effect! ~app ~key (fn ~args ~body)))
+
+(defn apply-effects! [app effects]
+  (doseq [[effect args] effects]
+    (let [f (-> app ::!effect-handlers deref effect)]
+      (apply f (partial handle! app) args))))
 
 (defn with-effect [state effect & args]
   (with-meta state (update (meta state) ::effects conj [effect args])))
+
+(defn without-effects [state]
+  (with-meta state (dissoc (meta state) ::effects)))
+
+(defn handle! [app key & args]
+  (let [f (-> app ::!event-handlers deref key)
+        state (swap! (::!state app) #(apply f % args))]
+    (apply-effects! app (-> state meta ::effects))
+    (swap! (::!state app) without-effects)
+    app))
 
 ;; Tests...
 
 (def state (atom {:val 0}))
 (def app (create state))
 
-(event! app :up #(update % :val inc))
-(event! app :down #(update % :val dec))
-(event! app :add (fn [state & args] (apply update state :val + args)))
-(event! app :slow-add-pi
-        (fn [state]
-          (-> state
-              (with-effect :compute-pi :add))))
+(defevent app :up [state]
+  (update state :val inc))
 
-(effect! app :compute-pi
-         (fn [handle! response-event]
-           (future (do (Thread/sleep 3000)
-                       (handle! response-event 3.14)
-                       (println "fired!")))))
+(defevent app :down [state]
+  (update state :val dec))
+
+(defevent app :add [state & args]
+  (apply update state :val + args))
+
+(defevent app :slow-add-pi [state]
+  (-> state
+      (with-effect :compute-pi :add)))
+
+(defeffect app :compute-pi [handle! response-event]
+  (future (do (Thread/sleep 3000)
+              (handle! response-event 3.14)
+              (println "fired!"))))
 
 (handle! app :up)
 (handle! app :add 5)
