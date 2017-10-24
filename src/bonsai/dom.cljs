e5be997d0e520e215e259a8c4e6b08072aaef175
Oliver Caldwell <olliec87@gmail.com>
2017-10-24 13:07:32 +0100

    Slowly growing to a point where it will consume the entire tree
    Basically building flat trees with DOM element information attached to each
    list of nodes. More efficient too since I only have to parse / check the new
    tree now, not new and old.
    
    Getting more stack based, just need to make sure I'm rolling nested :nodes /
    :children up into the stack. I think :nodes is pre and :children is post.

--- src/bonsai/dom.cljs
+++ src/bonsai/dom.cljs
@@ -8,25 +8,33 @@
                     :node (s/cat :name keyword?
                                  :children (s/* ::tree))))
 
-(defn parse [source]
+(defn start-stack [source]
   (let [result (s/conform ::tree source)]
     (if (s/invalid? result)
       (throw (js/Error. (expound/expound-str ::tree source)))
-      (if (= (first result) :nodes)
-        (second result)
-        [result]))))
+      [(if (= (first result) :nodes)
+         (second result)
+         [result])])))
 
-(defn render [mount {:keys [old new]}]
-  (loop [old-nodes (parse old)
-         new-nodes (parse new)]
-    (when (not= old-nodes new-nodes)
-      (doseq [[idx [new-type new-value]] (map-indexed vector new-nodes)]
-        (let [[old-type old-value] (nth old-nodes idx)]
-          (case new-type
-            :nothing (prn "DOING NOTHING" new-value)
-            :text (prn "HANDLING TEXT" new-value)
-            :nodes (prn "HIT A SEQ OF NODES" new-value)
-            :node (prn "HIT A NODE" new-value)))))))
+(defn parent [nodes]
+  (-> nodes meta :parent))
 
-;; So I should be able to handle the top level with this.
-;; To handle children and seqs of nodes, I'll need to concat and recur + attach some DOM node.
+(defn sync-node [parent old new idx]
+  (prn old new idx))
+
+(defn render [old-stack new-source mount]
+  (loop [old-stack old-stack
+         new-stack (start-stack new-source)
+         acc []]
+    (if (= old-stack new-stack nil)
+      acc
+      (let [[old-nodes & old-rest] old-stack
+            [new-nodes & new-rest] new-stack
+            parent-dom (or (parent old-nodes) mount)]
+        (doall (map (partial sync-node parent-dom) old-nodes new-nodes (range)))
+        (recur old-rest
+               new-rest
+               (conj acc (with-meta new-nodes {:parent parent-dom})))))))
+
+(-> (render nil [:p "hi"] :dom)
+    (render [:p "bye"] :dom))
