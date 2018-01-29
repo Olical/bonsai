b6261d4ad58727da057a255f4248339a850879e5
Oliver Caldwell <olliec87@gmail.com>
2018-01-29 19:15:56 +0000

    Started poking the API

--- test/bonsai/dom_test.cljs
+++ test/bonsai/dom_test.cljs
@@ -6,10 +6,16 @@
 (defn body []
   (-> (new jsdom/JSDOM) .-window .-document .-body))
 
-(t/deftest "essential rendering"
+(t/deftest rendering
   (t/testing "single p tag"
     (let [host (body)
           render (sut/mount {:host host
                              :render (fn []
                                        [:p "Hello, World!"])})]
+      (t/is (= (.-innerHTML host) "<p>Hello, World!</p>"))))
+
+  (t/testing "changing the text"
+    (let [host (body)
+          render (sut/mount {:host host
+                             :render (fn [text] [:p text])})]
       (t/is (= (.-innerHTML host) "<p>Hello, World!</p>")))))
