8619ee81bdf129189b3d19b9d1a1efae9d93bc11
Oliver Caldwell <olliec87@gmail.com>
2018-02-19 21:08:53 +0000

    Delete literally everthing, nuke it from orbit

--- test/bonsai/test_runner.cljs
+++
@@ -1,7 +0,0 @@
-(ns bonsai.test-runner
-  (:require [doo.runner :refer-macros [doo-tests]]
-            [bonsai.dom-test]
-            [bonsai.tree-test]))
-
-(doo-tests 'bonsai.dom-test
-           'bonsai.tree-test)
