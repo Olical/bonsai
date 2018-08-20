505eea79340459f9694326802d1a7e0d51b444f4
Oliver Caldwell <olliec87@gmail.com>
2018-08-20 13:29:38 +0100

    Slightly more patch scaffolding

--- src/bonsai/dom.cljs
+++ src/bonsai/dom.cljs
@@ -1,5 +1,10 @@
 (ns bonsai.dom
-  (:require [bonsai.tree :as tree]))
+  (:require [clojure.string :as str]
+            [bonsai.tree :as tree]))
 
-(defn patch! [node diff]
-  (aset node "innerHTML" (tree/->html (get-in diff [0 2]))))
+;; TODO Nodes need some sort of ID target on them for path->id. Although [0] should just be the host. Prolly for tree/->html.
+
+(defn patch! [host diff]
+  (doseq [[action path tree] diff]
+    (case action
+      :insert (aset host "innerHTML" (tree/->html tree)))))
